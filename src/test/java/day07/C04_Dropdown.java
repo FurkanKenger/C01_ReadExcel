3e2400ebd264dac0057f1f5af1c09424256da7ef
Furkan Kenger <kengermf@gmail.com>
2022-09-19 21:14:55 +0300

    V1

--- src/test/java/day07/C04_Dropdown.java
+++ src/test/java/day07/C04_Dropdown.java
@@ -1,7 +1,7 @@
 package day07;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,25 +9,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
 import java.time.Duration;
 import java.util.List;
-
-public class C04_DropDown {
-    /*
-    Bir class oluşturun: DropDown
-https://the-internet.herokuapp.com/dropdown adresine gidin.
-Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
-Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
-Visible Text(Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
-Tüm dropdown değerleri(value) yazdırın
-Dropdown’un boyutunu bulun, Dropdown’da 4 öğe varsa konsolda True , degilse
-False yazdırın.
-     */
+public class C04_Dropdown {
     WebDriver driver;
-    Select select;
     @Before
-    public void setUp() {
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -35,37 +22,43 @@ False yazdırın.
     }
     @After
     public void tearDown() {
-        driver.quit();
+        //driver.close();
     }
     @Test
-    public void test() {
-        // https://the-internet.herokuapp.com/dropdown adresine gidin.
+    public void test1(){
+        //https://the-internet.herokuapp.com/dropdown adresine gidin.
         driver.get("https://the-internet.herokuapp.com/dropdown");
         //Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
-        WebElement firstOption = driver.findElement(By.xpath("//*[@id = 'dropdown']"));
-        select = new Select(firstOption);
+        WebElement ddmList = driver.findElement(By.xpath("//*[@id='dropdown']"));
+        Select select = new Select(ddmList);
         select.selectByIndex(1);
+        //ddmList.sendKeys("Option 1");
         System.out.println(select.getFirstSelectedOption().getText());
+        System.out.println("=========================================");
         //Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
-        WebElement secondOption = driver.findElement(By.xpath("//*[@id = 'dropdown']"));
-        select = new Select(secondOption);
-        select.selectByIndex(2);
+        select.selectByValue("2");
         System.out.println(select.getFirstSelectedOption().getText());
+        System.out.println("=========================================");
         //Visible Text(Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
         select.selectByVisibleText("Option 1");
         System.out.println(select.getFirstSelectedOption().getText());
+        System.out.println("=========================================");
         //Tüm dropdown değerleri(value) yazdırın
-        List<WebElement> drops = select.getOptions();
-        for (WebElement w:drops
-        ) {
-            System.out.println(w.getText());
-        }
+        List<WebElement> butunDdm = driver.findElements(By.xpath("//option"));
+        butunDdm.forEach(t-> System.out.println(t.getText()));
+        System.out.println("=========================================");
+        //List <WebElement> tumDdm = select.getOptions();
+        //tumDdm.forEach(t-> System.out.println(t.getText()));
+        //for (WebElement w:tumDdm) {
+        //    System.out.println(w.getText());
+        //}
+
         //Dropdown’un boyutunu bulun, Dropdown’da 4 öğe varsa konsolda True , degilse
         //False yazdırın.
-        int actualOptionsNumbers = select.getOptions().size();
-        int expectedOptionsNumbers = 4;
-        if (actualOptionsNumbers == expectedOptionsNumbers) {
+        System.out.println("DropDown Boyutu = "+butunDdm.size());
+        if (butunDdm.size()==4){
             System.out.println("True");
-        } else System.out.println("False");
+        }else System.out.println("False");
+        Assert.assertNotEquals(butunDdm.size(),4);
     }
 }
